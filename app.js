f41d620d720a258a68759fce3fa83801a1032cfa
telarmago <telarmago@users.noreply.github.com>
2018-10-26 18:56:24 -0400

    Add more test endpoints

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/map', function(req, res) {
+  res.send('Map:');
+});
+
+app.get('/leaderboard', function(req, res) {
+  res.send('Leaderboard:');
+});
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
