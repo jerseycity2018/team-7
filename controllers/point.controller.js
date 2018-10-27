a36836f37fd7ba16e52481eb81ac33a73edb7662
AMinerva400 <AMinerva400@gmail.com>
2018-10-26 22:40:48 -0400

    Create Point

--- controllers/point.controller.js
+++ controllers/point.controller.js
@@ -3,3 +3,24 @@ const Point = require('../models/point.model');
 exports.test = function (req, res) {
 	res.send('Testing Point Controller');
 };
+
+exports.pointCreate = function (req, res) {
+	let point = new Point({
+		latitude: req.body.latitude,
+		longitude: req.body.longitude,
+		recycled: req.body.recycled,
+		img: req.body.img,
+		title: req.body.title,
+		body: req.body.body,
+		upVotes: req.body.upVotes,
+		time: req.body.time,
+		UID: req.body.UID,
+	});
+	
+	point.save(function (err) {
+		if(err){
+			return next(err);
+		}
+		res.send('Point Created Successfully!')
+	})
+};
--- routes/point.route.js
+++ routes/point.route.js
@@ -5,5 +5,7 @@ const pointController = require('../controllers/point.controller');
 
 //Testing Communications
 router.get('/test', pointController.test);
+//Create Point
+router.post('/create', pointController.pointCreate);
 
 module.exports = router;
