7eeea94fd951f7485c2da331b3ae9ed3db734973
reva3b <revathi3b@gmail.com>
2018-10-26 22:47:38 -0400

    Add Sticky Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import StickyFooter from 'react-sticky-footer';
 import SearchBar from './SearchBar';
 import logo from './logo.svg';
 import NavBar from './NavBar'
@@ -38,6 +39,19 @@ class App extends Component {
           <Map/>
           <SocialMedia/>
         </div>
+        <StickyFooter
+        bottomThreshold={50}
+        normalStyles={{
+          backgroundColor: "#999999",
+          padding: "2rem"
+        }}
+        stickyStyles={{
+          backgroundColor: "rgba(255,255,255,.8)",
+          padding: "2rem"
+          }}
+          >
+          Add any footer markup here
+          </StickyFooter>
       </div>
     );
   }
