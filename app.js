0d9701095cf6920de416426da4afca6cd3937b56
reva3b <revathi3b@gmail.com>
2018-10-26 20:42:04 -0400

    Revert "Merge branch 'master' of https://github.com/jerseycity2018/team-7"
    This reverts commit 746c21dc825fec97a7de2038e5e0ded79f9f6083, reversing
    changes made to 6b1c57bfeb8abfd996b8e87f0eeb58d58386f35a.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
 
 app.get('/', function(req, res) {
@@ -12,45 +11,6 @@ app.get('/map', function(req, res) {
 
 app.get('/leaderboard', function(req, res) {
   res.send('Leaderboard:');
-});
-
-app.get('/personal', function(req, res) {
-  res.send('Your personal stats:');
-});
-
-app.get('/socialmedia', function(req, res) {
-  res.send('Share to social media:');
-});
-
-app.get('/video', function(req, res) {
-  const path = 'media/test.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
 });
 
 app.listen(3000, function() {
