7eb28d4a31156841146c1a4d7a17d309eba65b4e
AMinerva400 <AMinerva400@gmail.com>
2018-10-26 21:23:55 -0400

    Folder Structure for Controller/Routes

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 var app = express();
 
+const Point = require('../routes/point.route');
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.get('/map', function(req, res) {
-  res.send('Map:');
+	res.send('Map:');
 });
 
 app.get('/leaderboard', function(req, res) {
-  res.send('Leaderboard:');
+	res.send('Leaderboard:');
 });
 
+//app.use('/points', Point);
+
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
