5dd957db95319ef715048c0ab42270e37acae769
telarmago <telarmago@users.noreply.github.com>
2018-10-26 22:50:17 -0400

    Add create function for User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,23 @@
-const Point = require('../models/user.model');
+const User = require('../models/user.model');
 
 exports.test = function (req, res) {
     res.send('Testing User Controller');
+};
+
+exports.product_create = function (req, res) {
+    var user = new User(
+        {
+            userID: req.body.userID,
+            name: req.body.name,
+            password: req.body.password,
+            location: req.body.location
+        }
+    );
+
+    user.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('User created successfully')
+    })
 };
